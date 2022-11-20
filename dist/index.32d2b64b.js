c0e4a385f64650e0382126de85782f253d3d89c3
LucasAndria <lucasandria01@gmail.com>
2022-11-20 09:53:08 +0300

    add comment

--- dist/index.32d2b64b.js
+++ dist/index.32d2b64b.js
@@ -688,7 +688,7 @@ exports.deleteRoute = (control)=>{
     map.removeControl(control);
 };
 
-},{"leaflet":"iFbO2","./leaflet":"xvuTT","./auto-complete":"5ZSXS","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","leaflet-routing-machine":"dzMVH"}],"iFbO2":[function(require,module,exports) {
+},{"leaflet":"iFbO2","leaflet-routing-machine":"dzMVH","./leaflet":"xvuTT","./auto-complete":"5ZSXS","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"iFbO2":[function(require,module,exports) {
 /* @preserve
  * Leaflet 1.9.2, a JS library for interactive maps. https://leafletjs.com
  * (c) 2010-2022 Vladimir Agafonkin, (c) 2010-2011 CloudMade
@@ -11185,133 +11185,6 @@ exports.deleteRoute = (control)=>{
     // Always export us to window global (see #2364)
     window.L = exports1;
 });
-
-},{}],"xvuTT":[function(require,module,exports) {
-exports.createLeafletInterface = (domId, options)=>{
-    const map = domId || "map";
-    const mapOptions = options || {
-        center: [
-            -18.933333,
-            47.516667
-        ],
-        zoom: 11
-    };
-    // Setup the map properties for leaflet
-    return L.map(map, mapOptions);
-};
-exports.addTiler = (map, TileLayer, options)=>{
-    // Add Tiles from openstreetmap if no Tiles given
-    const mapLayer = TileLayer || "https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}";
-    // Options like attributions
-    const mapOptions = options || {};
-    // Add tiles to the map
-    L.tileLayer(mapLayer, mapOptions).addTo(map);
-};
-exports.addMarker = (map, latlong, icon)=>{
-    const markerIcon = icon || {};
-    // Add marker
-    return L.marker(latlong, markerIcon).addTo(map);
-};
-exports.showPopup = (marker, text)=>{
-    const popup = text || "Hello...";
-    // Add popup
-    marker.bindPopup(popup).openPopup();
-};
-
-},{}],"5ZSXS":[function(require,module,exports) {
-module.exports = function autocomplete(inp, arr, showOnEmpty = false) {
-    /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/ var currentFocus;
-    /*execute a function when someone writes in the text field:*/ inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/ closeAllLists();
-        if (!val && !showOnEmpty) return false;
-        currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/ a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/ this.parentNode.appendChild(a);
-        /*for each item in the array...*/ for(i = 0; i < arr.length; i++)/*check if the item starts with the same letters as the text field value:*/ if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/ b = document.createElement("DIV");
-            /*make the matching letters bold:*/ b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/ b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/ b.addEventListener("click", function(e) {
-                /*insert the value for the autocomplete text field:*/ inp.value = this.getElementsByTagName("input")[0].value;
-                /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/ closeAllLists();
-            });
-            a.appendChild(b);
-        }
-    });
-    /*execute a function presses a key on the keyboard:*/ inp.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-            /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/ currentFocus++;
-            /*and and make the current item more visible:*/ addActive(x);
-        } else if (e.keyCode == 38) {
-            //up
-            /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/ currentFocus--;
-            /*and and make the current item more visible:*/ addActive(x);
-        } else if (e.keyCode == 13) {
-            /*If the ENTER key is pressed, prevent the form from being submitted,*/ e.preventDefault();
-            if (currentFocus > -1) /*and simulate a click on the "active" item:*/ {
-                if (x) x[currentFocus].click();
-            }
-        }
-    });
-    function addActive(x) {
-        /*a function to classify an item as "active":*/ if (!x) return false;
-        /*start by removing the "active" class on all items:*/ removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = x.length - 1;
-        /*add class "autocomplete-active":*/ x[currentFocus].classList.add("autocomplete-active");
-    }
-    function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/ for(var i = 0; i < x.length; i++)x[i].classList.remove("autocomplete-active");
-    }
-    function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/ var x = document.getElementsByClassName("autocomplete-items");
-        for(var i = 0; i < x.length; i++)if (elmnt != x[i] && elmnt != inp) x[i].parentNode.removeChild(x[i]);
-    }
-    /*execute a function when someone clicks in the document:*/ document.addEventListener("click", function(e) {
-        closeAllLists(e.target);
-    });
-};
-
-},{}],"gkKU3":[function(require,module,exports) {
-exports.interopDefault = function(a) {
-    return a && a.__esModule ? a : {
-        default: a
-    };
-};
-exports.defineInteropFlag = function(a) {
-    Object.defineProperty(a, "__esModule", {
-        value: true
-    });
-};
-exports.exportAll = function(source, dest) {
-    Object.keys(source).forEach(function(key) {
-        if (key === "default" || key === "__esModule" || dest.hasOwnProperty(key)) return;
-        Object.defineProperty(dest, key, {
-            enumerable: true,
-            get: function() {
-                return source[key];
-            }
-        });
-    });
-    return dest;
-};
-exports.export = function(dest, destName, get) {
-    Object.defineProperty(dest, destName, {
-        enumerable: true,
-        get: get
-    });
-};
 
 },{}],"dzMVH":[function(require,module,exports) {
 var global = arguments[3];
@@ -32898,6 +32771,133 @@ var global = arguments[3];
 }, {}, [
     53
 ]);
+
+},{}],"xvuTT":[function(require,module,exports) {
+exports.createLeafletInterface = (domId, options)=>{
+    const map = domId || "map";
+    const mapOptions = options || {
+        center: [
+            -18.933333,
+            47.516667
+        ],
+        zoom: 11
+    };
+    // Setup the map properties for leaflet
+    return L.map(map, mapOptions);
+};
+exports.addTiler = (map, TileLayer, options)=>{
+    // Add Tiles from openstreetmap if no Tiles given
+    const mapLayer = TileLayer || "https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}";
+    // Options like attributions
+    const mapOptions = options || {};
+    // Add tiles to the map
+    L.tileLayer(mapLayer, mapOptions).addTo(map);
+};
+exports.addMarker = (map, latlong, icon)=>{
+    const markerIcon = icon || {};
+    // Add marker
+    return L.marker(latlong, markerIcon).addTo(map);
+};
+exports.showPopup = (marker, text)=>{
+    const popup = text || "Hello...";
+    // Add popup
+    marker.bindPopup(popup).openPopup();
+};
+
+},{}],"5ZSXS":[function(require,module,exports) {
+module.exports = function autocomplete(inp, arr, showOnEmpty = false) {
+    /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/ var currentFocus;
+    /*execute a function when someone writes in the text field:*/ inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/ closeAllLists();
+        if (!val && !showOnEmpty) return false;
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/ a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/ this.parentNode.appendChild(a);
+        /*for each item in the array...*/ for(i = 0; i < arr.length; i++)/*check if the item starts with the same letters as the text field value:*/ if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/ b = document.createElement("DIV");
+            /*make the matching letters bold:*/ b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/ b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/ b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/ inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/ closeAllLists();
+            });
+            a.appendChild(b);
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/ inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/ currentFocus++;
+            /*and and make the current item more visible:*/ addActive(x);
+        } else if (e.keyCode == 38) {
+            //up
+            /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/ currentFocus--;
+            /*and and make the current item more visible:*/ addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/ e.preventDefault();
+            if (currentFocus > -1) /*and simulate a click on the "active" item:*/ {
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/ if (!x) return false;
+        /*start by removing the "active" class on all items:*/ removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = x.length - 1;
+        /*add class "autocomplete-active":*/ x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/ for(var i = 0; i < x.length; i++)x[i].classList.remove("autocomplete-active");
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/ var x = document.getElementsByClassName("autocomplete-items");
+        for(var i = 0; i < x.length; i++)if (elmnt != x[i] && elmnt != inp) x[i].parentNode.removeChild(x[i]);
+    }
+    /*execute a function when someone clicks in the document:*/ document.addEventListener("click", function(e) {
+        closeAllLists(e.target);
+    });
+};
+
+},{}],"gkKU3":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, "__esModule", {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === "default" || key === "__esModule" || dest.hasOwnProperty(key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
 
 },{}]},["dhFGg","f2QDv"], "f2QDv", "parcelRequirecbe0")
 
