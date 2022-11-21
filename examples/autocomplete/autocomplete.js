f5bd9faafe1265c3f27a839ee1c2d5b245960f80
LucasAndria <lucasandria01@gmail.com>
2022-11-21 19:15:36 +0300

    [ADD] comments on autocomplete usage

--- examples/autocomplete/autocomplete.js
+++ examples/autocomplete/autocomplete.js
@@ -8,5 +8,14 @@ const stores = [
   "my store andravohangy",
 ];
 
+/* 
+  it add the elements in the array as a choice as the user input
+  first params : id of the div that wrap the input element
+  if there are many elements with that id, then it will take the first
+  second params : array of string used for the autocomplete
+  third params: boolean to display all choice when the input field is empty
+*/
+
 autocomplete("ac-countries", countries);
+
 autocomplete("ac-stores", stores, true);
