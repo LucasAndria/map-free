868802669d6dccfe4f9a2e35f0dee4e9afa0388d
LucasAndria <lucasandria01@gmail.com>
2022-11-22 22:09:56 +0300

    [ADD] geocoding files

---
+++ src/geocoding.js
@@ -0,0 +1,9 @@
+// https://api.openrouteservice.org/geocode/autocomplete?api_key=5b3ce3597851110001cf624876a8d59872974c48b862c6ff043b5745&text=Toky
+
+const geo_autocomplete = async (text) => {
+  try {
+    axios;
+  } catch {}
+};
+
+module.exports = function () {};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import autocomplete from "./autocomplete";
+import geocoding from "./geocoding.js";
 
 exports.autocomplete = (containerId, array, showOnEmpty) => {
   autocomplete(containerId, array, showOnEmpty);
 };
+
+exports.geocoding = geocoding;
