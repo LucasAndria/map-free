e72ede0b70e9474ec71bec8d52dc1e636e572eae
LucasAndria <lucasandria01@gmail.com>
2022-11-23 13:12:33 +0300

    [ADD] test geocoding and autocomplete

--- examples/geocoding/index.js
+++ examples/geocoding/index.js
@@ -1,1 +1,30 @@
 import "leaflet";
+import { geocoding, autocomplete } from "../../src/index";
+
+const formGeocoding = document.querySelector(".form-geocoding");
+
+(async () => {
+  const input = document.getElementById("recherche_lieu");
+  let data = { labels: [] };
+  input.addEventListener("input", async () => {
+    data = await geocoding();
+    autocomplete("auto_lieu", data.labels);
+  });
+})();
+
+// if (formGeocoding) {
+//   const input = document.getElementById("recherche_lieu");
+
+//   let autocomplete_timeout;
+
+//   input.addEventListener("input", () => {
+//     if (autocomplete_timeout) clearTimeout(autocomplete_timeout);
+
+//     autocomplete_timeout = setTimeout(async () => {
+//       const data = await geocoding();
+
+//       console.log("ok");
+//       autocomplete("auto_lieu", data.labels);
+//     }, 1000);
+//   });
+// }
